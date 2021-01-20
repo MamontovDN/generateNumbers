e074fbc52362bd671f3806d32143ce04bf9798be
MamontovDN <mamontovdn@yandex.ru>
2021-01-20 08:55:35 +0300

    onopen event for socket and recieve for consumer

--- number/consumers.py
+++ number/consumers.py
@@ -2,6 +2,8 @@ import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+from number.number_generator import MY_NUMBER
+
 
 class NumConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -10,6 +12,9 @@ class NumConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard('gr_num', self.channel_name)
+
+    async def receive(self, text_data=None):
+        await self.send(str(MY_NUMBER[0]))
 
     async def send_num(self, event):
         number = event['message']
--- number/number_generator.py
+++ number/number_generator.py
@@ -6,12 +6,17 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from channels.layers import get_channel_layer
 
 channel_layer = get_channel_layer()
+MY_NUMBER = [1]
 
 
 def job():
-    n = random.randint(100, 1000)
-    async_to_sync(channel_layer.group_send)('gr_num', {'type': 'send_num',
-                                                       'message': str(n)})
+    global MY_NUMBER
+    MY_NUMBER[0] = random.randint(100, 1000)
+    async_to_sync(channel_layer.group_send)('gr_num',
+                                            {
+                                                'type': 'send_num',
+                                                'message': str(MY_NUMBER[0])
+                                            })
 
 
 def start():
